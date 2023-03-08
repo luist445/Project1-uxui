30db0ffe636dc7874110b075bd3408f08fcde77b
luist445 <luis.tepozvazquez84@myhunter.cuny.edu>
2023-03-08 17:06:42 -0500

    Adding new links + testing out merge feature again

---
+++ projecto1/src/components/Attractions.js
@@ -0,0 +1,12 @@
+//import './Attractions.css'
+
+function Attractions()
+{
+    return(
+        <div className="content">
+            The three attractions of One Piece!
+        </div>
+    );
+}
+
+export default Attractions;
